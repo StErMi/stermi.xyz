ee7fe8c34e9721143f051a41cd0b3ba496630b63
StErMi <stermi@gmail.com>
2022-10-26 18:12:51 +0200

    Added support to Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
+
 import 'focus-visible'
 import '../styles/index.css'
 import 'prismjs/themes/prism-tomorrow.css'
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
