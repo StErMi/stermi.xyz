a318f510874811bb3dfb27d229ad2bec9e7be5ca
StErMi <stermi@gmail.com>
2023-09-01 08:54:48 +0200

    Added support to markdown table

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,12 +1,14 @@
-import { remark } from 'remark';
-import html from 'remark-html';
-import prism from 'remark-prism';
+import { remark } from 'remark'
+import html from 'remark-html'
+import prism from 'remark-prism'
+import remarkGfm from 'remark-gfm'
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
     // https://github.com/sergioramos/remark-prism/issues/265
     .use(html, { sanitize: false })
+    .use(remarkGfm)
     .use(prism)
-    .process(markdown);
-  return result.toString();
+    .process(markdown)
+  return result.toString()
 }
